1609aa2018ab060b3bbf51345b8bc999ff761c5f
francianepovoa <francianerpovoa@gmail.com>
2021-02-24 17:25:54 +0100

    Implemantation countdown JS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,22 +9,20 @@ import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
 	return (
-		<div>
-			<div className={styles.container}>
-				<Head>
-					<title>Start | move.it </title>
-				</Head>
+		<div className={styles.container}>
+			<Head>
+				<title>Start | move.it </title>
+			</Head>
 
-				<ExperienceBar />
-				<section>
-					<div>
-						<Profile />
-						<CompletedChallenges />
-						<Countdown />
-					</div>
-					<div></div>
-				</section>
-			</div>
+			<ExperienceBar />
+			<section>
+				<div>
+					<Profile />
+					<CompletedChallenges />
+					<Countdown />
+				</div>
+				<div></div>
+			</section>
 		</div>
 	);
 }
