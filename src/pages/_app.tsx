db5a9c35d1ef59ffa5fb3cabb9c9e730bf1e42c4
francianepovoa <francianerpovoa@gmail.com>
2021-02-25 18:13:28 +0100

    Aplication Context API: notification

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChallengesProvider } from "../context/ChallengesContext";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
@@ -10,3 +10,22 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
+//* #Rumoaoproximonivel (primeiro dia)
+//? No primeiro dia vamos conhecer os conceitos por trás de um front-end SPA, os principais fundamentos do React
+//? como componentes, propriedades, estado e ferramentas que funcionam por trás da biblioteca como Webpack e Babel.
+//? Além disso, daremos início ao desenvolvimento da aplicação move.it, criando a estrutura básica de componentes da página.
+
+//* #jornadainfinita (segundo dia)
+//? Nesse segundo dia vamos aprender o que é Next.js e quando utilizar no desenvolvimento de aplicações React.
+//? Vamos também criar nossos próprios hooks dando vida às funcionalidades de countdown e
+//? ganho de experiência e leveis através de desafios.
+
+//* #focopraticagrupo (terceiro dia)
+//? Na terceiro aula realizaremos uma separação da aplicação em componentes menores e
+//? aplicaremos o conceito de contextos no React, responsável por compartilhar uma informação entre vários componentes em tela.
+//? Finalizaremos também a funcionalidade de desafios.
+
+//* #neverstoplearning (quarto dia)
+//? Na quarta aula iremos continuar evoluindo nossa aplicação, aprender ainda mais sobre a Context API,
+//? e aprender a como utilizar as notificações e sons do navegador para melhorar a experiência do usuário.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { CountdownProvider } from "../contexts/CountdownContext";
 
 import styles from "../styles/pages/Home.module.css";
 
@@ -16,35 +17,18 @@ export default function Home() {
 			</Head>
 
 			<ExperienceBar />
-			<section>
-				<div>
-					<Profile />
-					<CompletedChallenges />
-					<Countdown />
-				</div>
-				<div>
-					<ChallengeBox />
-				</div>
-			</section>
+			<CountdownProvider>
+				<section>
+					<div>
+						<Profile />
+						<CompletedChallenges />
+						<Countdown />
+					</div>
+					<div>
+						<ChallengeBox />
+					</div>
+				</section>
+			</CountdownProvider>
 		</div>
 	);
 }
-
-//* #Rumoaoproximonivel (primeiro dia)
-//? No primeiro dia vamos conhecer os conceitos por trás de um front-end SPA, os principais fundamentos do React
-//? como componentes, propriedades, estado e ferramentas que funcionam por trás da biblioteca como Webpack e Babel.
-//? Além disso, daremos início ao desenvolvimento da aplicação move.it, criando a estrutura básica de componentes da página.
-
-//* #jornadainfinita (segundo dia)
-//? Nesse segundo dia vamos aprender o que é Next.js e quando utilizar no desenvolvimento de aplicações React.
-//? Vamos também criar nossos próprios hooks dando vida às funcionalidades de countdown e
-//? ganho de experiência e leveis através de desafios.
-
-//* #focopraticagrupo (terceiro dia)
-//? Na terceiro aula realizaremos uma separação da aplicação em componentes menores e
-//? aplicaremos o conceito de contextos no React, responsável por compartilhar uma informação entre vários componentes em tela.
-//? Finalizaremos também a funcionalidade de desafios.
-
-//* #(quarto dia)
-//? Na quarta aula iremos continuar evoluindo nossa aplicação, aprender ainda mais sobre a Context API,
-//? e aprender a como utilizar as notificações e sons do navegador para melhorar a experiência do usuário.
