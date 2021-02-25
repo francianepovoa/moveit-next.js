f5832af549168f5b435f2aefec28598e2701d257
francianepovoa <francianerpovoa@gmail.com>
2021-02-25 16:21:23 +0100

    Aplication Context API

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../context/ChallengesContext";
 import styles from "../styles/components/CompletedChallenges.module.css";
 
 export function CompletedChallenges() {
+	const { challengesCompleted } = useContext(ChallengesContext);
+
 	return (
 		<div className={styles.completedChallengeContainer}>
-			<span>Complete Challenges</span>
-			<span>5</span>
+			<span>Completed Challenges</span>
+			<span>{challengesCompleted}</span>
 		</div>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import Head from "next/head";
+
+import { ChallengeBox } from "../components/ChallengeBox";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-
-import Head from "next/head";
 
 import styles from "../styles/pages/Home.module.css";
 
@@ -21,10 +22,29 @@ export default function Home() {
 					<CompletedChallenges />
 					<Countdown />
 				</div>
-				<div></div>
+				<div>
+					<ChallengeBox />
+				</div>
 			</section>
 		</div>
 	);
 }
 
-// #jornadainfinita
+//* #Rumoaoproximonivel (primeiro dia)
+//? No primeiro dia vamos conhecer os conceitos por trás de um front-end SPA, os principais fundamentos do React
+//? como componentes, propriedades, estado e ferramentas que funcionam por trás da biblioteca como Webpack e Babel.
+//? Além disso, daremos início ao desenvolvimento da aplicação move.it, criando a estrutura básica de componentes da página.
+
+//* #jornadainfinita (segundo dia)
+//? Nesse segundo dia vamos aprender o que é Next.js e quando utilizar no desenvolvimento de aplicações React.
+//? Vamos também criar nossos próprios hooks dando vida às funcionalidades de countdown e
+//? ganho de experiência e leveis através de desafios.
+
+//* #focopraticagrupo (terceiro dia)
+//? Na terceiro aula realizaremos uma separação da aplicação em componentes menores e
+//? aplicaremos o conceito de contextos no React, responsável por compartilhar uma informação entre vários componentes em tela.
+//? Finalizaremos também a funcionalidade de desafios.
+
+//* #(quarto dia)
+//? Na quarta aula iremos continuar evoluindo nossa aplicação, aprender ainda mais sobre a Context API,
+//? e aprender a como utilizar as notificações e sons do navegador para melhorar a experiência do usuário.
